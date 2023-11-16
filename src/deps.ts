9eb7ae313cfb85bec45530e60f4d952bfbdf2551
sgohlke <gohlke.stefan@yahoo.de>
2023-11-17 00:01:20 +0100

    Update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,9 @@
-export { assert } from 'https://deno.land/std@0.202.0/assert/assert.ts'
+export { assert } from 'https://deno.land/std@0.206.0/assert/assert.ts'
 
 export {
    assertEquals,
-} from 'https://deno.land/std@0.202.0/assert/assert_equals.ts'
+} from 'https://deno.land/std@0.206.0/assert/assert_equals.ts'
 
-export { fail } from 'https://deno.land/std@0.202.0/assert/fail.ts'
+export { fail } from 'https://deno.land/std@0.206.0/assert/fail.ts'
 
 export * from 'https://raw.githubusercontent.com/sgohlke/rpg-ts-base/v0.4.0/src/index.ts'
