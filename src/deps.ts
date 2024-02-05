e10041a40eb0e60b9c078e5c3b204025e7e3d6fc
sgohlke <gohlke.stefan@yahoo.de>
2024-02-05 22:32:41 +0100

    Update deno-web lib

--- src/deps.ts
+++ src/deps.ts
@@ -8,6 +8,6 @@ export {
    createPasswordHash,
    generateAccessTokenHash,
    verifyPassword,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/crypto/cryptoFunctions.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.1/crypto/cryptoFunctions.ts'
 
 export * from 'https://raw.githubusercontent.com/sgohlke/rpg-ts-base/v0.4.0/src/index.ts'
