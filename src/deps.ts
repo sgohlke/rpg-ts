37cc4fdefee6989cfb9c0ac21304e47fc31068b6
sgohlke <gohlke.stefan@yahoo.de>
2024-02-05 22:18:12 +0100

    Update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,14 +1,13 @@
 export {
    assert,
    assertEquals,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/deps.ts'
+   fail
+} from 'https://raw.githubusercontent.com/sgohlke/deno-deps/main/deps.ts'
 
 export {
    createPasswordHash,
    generateAccessTokenHash,
    verifyPassword,
 } from 'https://raw.githubusercontent.com/sgohlke/deno-web/8.2.0/crypto/cryptoFunctions.ts'
-
-export { fail } from 'https://deno.land/std@0.212.0/assert/fail.ts'
 
 export * from 'https://raw.githubusercontent.com/sgohlke/rpg-ts-base/v0.4.0/src/index.ts'
